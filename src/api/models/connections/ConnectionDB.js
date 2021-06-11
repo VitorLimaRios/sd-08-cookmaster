81503067b02212819fa76041e00333f53c6c599c
Digo-Castro <digo_castro@outlook.com>
2021-06-11 15:11:16 -0300

    [Digo-Castro][Cookmaster] - Init

---
+++ src/api/models/connections/ConnectionDB.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb');
+const { MONGO_DB_URL_LOCAL, OPTIONS, DB_NAME } = require('../../utils/consts');
+
+const connectionDB = () => {
+  return MongoClient.connect(MONGO_DB_URL_LOCAL, OPTIONS)
+    .then((conn) => console.log('Connected to the MongoDB.') || conn.db(DB_NAME))
+    .catch((err) => {
+      console.log(err);
+      process.exit();
+    });
+};
+
+module.exports = connectionDB;
---
+++ src/api/utils/consts.js
@@ -0,0 +1,20 @@
+const COLLECTION_ADMINS = 'admins';
+const COLLECTION_RECIPES = 'recipes';
+const COLLECTION_USERS = 'users';
+const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL_LOCAL = 'mongodb://localhost:27017/Cookmaster';
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+module.exports = {
+  COLLECTION_ADMINS,
+  COLLECTION_RECIPES,
+  COLLECTION_USERS,
+  DB_NAME,
+  MONGO_DB_URL,
+  MONGO_DB_URL_LOCAL,
+  OPTIONS,
+};
