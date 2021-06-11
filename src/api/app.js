2ed6776ad6edc0c5acf1e95534c219124afb3a4d
liisaambrosen <liisa.ambrosen@gmail.com>
2021-06-11 15:04:05 -0300

    criando estrutura de arquivos

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const controller = require('./controllers/productController');
+const controllerSales = require('./controllers/salesController');
+const bodyParser = require('body-parser');
+const router = require('./routes/router');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(route);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
