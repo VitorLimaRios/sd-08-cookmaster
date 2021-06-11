ba0d47b39fc39ede1cd2f13cfa2667fbc0dc8291
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-06-11 16:02:19 -0300

    adiciona express.json ao app

--- src/api/app.js
+++ src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRoutes = require('../routes/usersRoutes');
 const app = express();
 
+app.use(express.json());
 app.use(usersRoutes);
 // Não remover esse end-point, ele é necessário para o avaliador.
 app.get('/', (request, response) => {
