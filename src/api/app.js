0d336d3940b34c3f0dc90c96ab5bfbd168edef2a
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-06-11 18:41:07 -0300

    feat: add folders

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const middleware = require('../middlewares');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
-// Não remover esse end-point, ele é necessário para o avaliador
+const userController = require('../controllers/userController');
+
+app.use(bodyParser.json());
+
+app.get('/', (_request, response) => response.send());
+
+app.post('/users', userController.createUser);
+
+app.use(middleware.error);
 
 module.exports = app;
