053383a4e9c560031e6168accd1a4a5c743bc78c
Anderson Santana <anderslash@gmail.com>
2021-06-11 20:28:48 -0300

    feat: valida se email ja existe

--- src/middlewares/validateUsers.js
+++ src/middlewares/validateUsers.js
@@ -1,14 +1,20 @@
 const validator = require('email-validator');
+const users = require('../models/user');
 
 const BAD_REQUEST = 400;
+const CONFLIT = 409;
 const INVALID_ENTRIES = {
   'message': 'Invalid entries. Try again.'
 };
+const EMAIL_EXISTS = {
+  'message': 'Email already registered'
+};
 
-const validateUsers = (req, res, next) => {
+const validateUsers = async (req, res, next) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) return res.status(BAD_REQUEST).json(INVALID_ENTRIES);
   if (!validator.validate(email)) return res.status(BAD_REQUEST).json(INVALID_ENTRIES);
+  if (await users.findEmail(email)) return res.status(CONFLIT).json(EMAIL_EXISTS);
   next();
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -4,11 +4,16 @@ const getAll = async () =>
   connection().then ((db) => db.collection('users')
     .find().toArray());
 
-const create = async(name, email, password, role) =>
+const create = async (name, email, password, role) =>
   connection().then ((db) => db.collection('users')
     .insertOne({ name, email, password, role }));
+
+const findEmail = async (email) =>
+  connection().then ((db) => db.collection('users')
+    .findOne({ 'email': email }));
 
 module.exports = {
   getAll,
   create,
+  findEmail,
 };
