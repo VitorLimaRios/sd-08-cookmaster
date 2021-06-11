86f3e6df8df3e7f592b2c158c6d8ffec69189494
JRGCast <joao.ralph1@gmail.com>
2021-06-11 15:27:01 -0300

    adicionado express.static, substituido o path.join por path.resolve

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+// ...
+
+// /images é o caminho/end-point da API onde as imagens estarão disponíveis
+// path.join(__dirname, '..', 'uploads') é o caminho da pasta onde o multer deve salvar suas imagens ao realizar o upload
+// a pasta `uploads` está em `./src/uploads` e não deve ser renomeada ou removida (assim como o arquivo `ratinho.jpg`)
+app.use('/images', express.static(path.resolve('src', 'uploads')));
+
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (_request, response) => {
