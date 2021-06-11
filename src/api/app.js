851d318f8fc955989bd49716c9ff7b96befc3936
Bruno Martins <bruno.martins217@gmail.com>
2021-06-11 20:27:00 -0300

    Desafio 1

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { createUser } = require('../controllers/userController');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.route('/user')
+  .post(createUser);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
