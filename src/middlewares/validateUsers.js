ee6a36c234100b03e049552434c08d9219936d73
Anderson Santana <anderslash@gmail.com>
2021-06-11 20:12:21 -0300

    fix: valida email com email-validator

--- src/middlewares/validateUsers.js
+++ src/middlewares/validateUsers.js
@@ -1,17 +1,14 @@
+const validator = require('email-validator');
+
 const BAD_REQUEST = 400;
 const INVALID_ENTRIES = {
   'message': 'Invalid entries. Try again.'
 };
 
-const testEmail = (email) => {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(email);
-};
-
 const validateUsers = (req, res, next) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) return res.status(BAD_REQUEST).json(INVALID_ENTRIES);
-  if (!testEmail(email)) return res.status(BAD_REQUEST).json(INVALID_ENTRIES);
+  if (!validator.validate(email)) return res.status(BAD_REQUEST).json(INVALID_ENTRIES);
   next();
 };
 
