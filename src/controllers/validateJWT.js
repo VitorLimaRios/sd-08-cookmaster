17c2da5693c5c3e5f21d2cc447a4555de33939e7
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-06-12 10:54:41 -0300

    adiciona condicional no controller de validaçao do JWT

--- src/controllers/validateJWT.js
+++ src/controllers/validateJWT.js
@@ -6,8 +6,14 @@ const UNAUTHORIZED = 401;
 const secret = 'publicSecretParadox';
 
 module.exports = rescue(async (req, res, next) => {
+  console.log(req.method, req.path);
   const token = req.headers.authorization;
+
+  if(!token && req.method === 'PUT' && req.path.includes('/recipes')) return res
+    .status(UNAUTHORIZED).json({ message: 'missing auth token' });
+
   if(!token) return res.status(UNAUTHORIZED).json({ message: 'jwt malformed' });
+
   try {
     const decoded = jwt.verify(token, secret);
     const user = await UsersModel.findById(decoded.id);
