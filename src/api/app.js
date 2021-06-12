8cf452abffff7499227e42b392f33f5fc3d2fa7a
Victor Miwa <victoryukimiwa@hotmail.com>
2021-06-12 00:31:53 -0300

    Desafio 10

--- src/api/app.js
+++ src/api/app.js
@@ -8,7 +8,7 @@ const middlewares = require('../middlewares');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/image', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
