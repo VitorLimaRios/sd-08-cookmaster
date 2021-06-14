0e6164aa71d5ed86fcb2961d3b4ec57c7746e14b
hugobrr <hugobrr@gmail.com>
2021-06-14 19:34:44 -0300

    1 requisito

--- src/services/user/createUsers.js
+++ src/services/user/createUsers.js
@@ -10,6 +10,7 @@ const validateEmail = require('../../validate/validateEmail');
 const HTTP_OK_STATUS = 201;
 // const ERRO_22 = 422;
 const ERRO_04 = 400;
+const ERRO_09 = 409;
 const ZERO = 0;
 const createSales = async (data, res , next)=>{
   const {name, email, password} = data;
@@ -28,7 +29,7 @@ const createSales = async (data, res , next)=>{
     console.log('lista de email',emailList ,email);
     validateEmail(email,emailList,res, msgError);
   }
-  if(res.statusCode!==ERRO_04){
+  if(res.statusCode!==ERRO_04 && res.statusCode!==ERRO_09){
     result =  await createDB('users',  {name,email, 'role': 'user'});
     res =  res.status(HTTP_OK_STATUS).json({user: result[0]});
     console.log('resultado', result[0]);
