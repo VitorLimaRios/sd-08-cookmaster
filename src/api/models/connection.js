5677daf9db17125cfb953330f0eba69d9731cf39
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-06-15 14:28:23 -0300

    connection - fix DB_URL

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -5,7 +5,7 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 let db = null;
 
