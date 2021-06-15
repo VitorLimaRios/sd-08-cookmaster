8fc5c4e79f489375e1397b554f2f07d87c381782
Digo-Castro <digo_castro@outlook.com>
2021-06-15 13:16:04 -0300

    [Requisito 8] - Error Timeout - FIXES URL

--- src/api/connections/ConnectionDB.js
+++ src/api/connections/ConnectionDB.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
-const { MONGO_DB_URL_LOCAL, OPTIONS, DB_NAME } = require('../utils/consts');
+const { MONGO_DB_URL_LOCAL, OPTIONS, DB_NAME, MONGO_DB_URL } = require('../utils/consts');
 
 const connectionDB = () => {
-  return MongoClient.connect(MONGO_DB_URL_LOCAL, OPTIONS)
+  return MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => console.log('Connected to the MongoDB.') || conn.db(DB_NAME))
     .catch((err) => {
       console.log(err);
