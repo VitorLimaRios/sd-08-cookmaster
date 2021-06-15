66af3c859c2c1a51c0144336bc6f432c68749225
Fernanda PC <nandateixmarques@gmail.com>
2021-06-15 13:53:04 -0300

    adicionando requisito8 passando no teste

--- src/middlewares/validateJWT.js
+++ src/middlewares/validateJWT.js
@@ -1,22 +1,20 @@
 const jwt = require('jsonwebtoken');
 const usersModel = require('../models/usersModel');
-const { code, message } = require('../helpers/messages');
+const { code } = require('../helpers/messages');
 
 const secret = 'seusecretdetoken';
 
 const validateJWT = async (req, res, next) => {
   const token = req.headers.authorization;
   try {
-    if (!token) throw new Error({ message:'missing auth token' });
+    if (!token) throw new Error('missing auth token');
     const decoded = jwt.verify(token, secret);
     const user = await usersModel.findUserByEmail(decoded.email);
-    if (!user) return res.status(code.UNAUTHORIZED).json(
-      { message: 'jwt malformed' }
-    );
+    if (!user) throw new Error('jwt malformed');
     req.user = user;
     next();
   } catch (error) {
-    return res.status(code.UNAUTHORIZED).json({ message: 'jwt malformed' });
+    return res.status(code.UNAUTHORIZED).json({ message: error.message });
   }
 };
 
