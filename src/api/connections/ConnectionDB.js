9381204d486c79374476670dfa1e3646b89891ad
Digo-Castro <digo_castro@outlook.com>
2021-06-15 18:31:02 -0300

    [Requisito 8] - ok - Fixes URL

--- src/api/connections/ConnectionDB.js
+++ src/api/connections/ConnectionDB.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const { MONGO_DB_URL_LOCAL, OPTIONS, DB_NAME, MONGO_DB_URL } = require('../utils/consts');
 
 const connectionDB = () => {
-  return MongoClient.connect(MONGO_DB_URL_LOCAL, OPTIONS)
+  return MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => console.log('Connected to the MongoDB.') || conn.db(DB_NAME))
     .catch((err) => {
       console.log(err);
