b07c7ee3662b09bbbb3039022697172c3932b565
Tiago <tiagaoalb@gmail.com>
2021-06-15 15:55:01 -0300

    add 1st requirement

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,17 +1,19 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL;
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+
+DB_NAME='Cookmaster';
 
 const connection = () => {
   return MongoClient
     .connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .catch((err) => {
       console.error(err);
       process.exit(1);
