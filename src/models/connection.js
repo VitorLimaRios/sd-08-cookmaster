20270fc6a80a23c9c9fd988a8acc7d8f96db775b
victor718 <victorfelipeoliveira1@gmail.com>
2021-06-14 21:09:54 -0300

    alter connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 
 const connectionMongo = async () => {
-  // const url = 'mongodb://mongodb:27017/Cookmaster';
-  const url = 'mongodb://localhost:27017/';
+  const url = 'mongodb://mongodb:27017/Cookmaster';
+  // const url = 'mongodb://localhost:27017/';
   const dbName = 'Cookmaster';
   try {
     const conn = await MongoClient.connect(url, {
