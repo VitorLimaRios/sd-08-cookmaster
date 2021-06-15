912dcf0f8bfa73aba8bbe4ef704162a22570ccc4
Mateus Coury <mateuscoury@gmail.com>
2021-06-15 16:10:04 -0300

    first commit

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const multer = require('multer');
+
+app.use(bodyParser.json());
+
+
+const storage = multer.diskStorage({
+  destination:(callback)=>callback(null,'uploads'),
+  filename:(callback)=>callback(null,'zin')
+});
+
+
+const upload = multer(storage);
+
+
+
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+
 
 module.exports = app;
