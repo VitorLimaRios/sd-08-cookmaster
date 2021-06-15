0e88e52ebc9d025270e43faa181abaf68225efe6
jnpefo <jnpefo@gmail.com>
2021-06-15 19:00:01 -0300

    Req 6

--- src/api/validations/validLogin.js
+++ src/api/validations/validLogin.js
@@ -2,7 +2,6 @@ const joi = require('joi');
 
 const msg = 'All fields must be filled';
 const msg1 = 'Incorrect username or password';
-const EIGHT = 8;
 
 const validLogin = joi.
   object({
@@ -17,11 +16,9 @@ const validLogin = joi.
       .required(),
     password: joi
       .string()
-      .min(EIGHT)
       .messages({ 
         'string.base': msg,
         'string.empty': msg,
-        'string.min': msg1,
         'any.required': msg,
       })
       .required(),
