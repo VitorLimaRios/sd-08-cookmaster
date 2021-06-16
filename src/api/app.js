bf13cb6562b4c622f18334a1b6e1e5469873eca5
Ediberto Oliveira <edibertooliveira@aol.com>
2021-06-16 15:50:14 -0300

    feat: added requeriment 10

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const signUpRouter = require('../router/user.router');
 const signInRouter = require('../router/login.router');
 const recipeRouter = require('../router/recipe.router');
@@ -7,14 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/recipes', recipeRouter);
-app.use('/users', signUpRouter);
-app.use('/login', signInRouter);
-
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (req, res) => {
   res.send();
 });
+
+app.use('/recipes', recipeRouter);
+app.use('/users', signUpRouter);
+app.use('/login', signInRouter);
+app.use('/images/', express.static(path.join(__dirname, '/../uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 
