dd58d0f2c07101f08b870f6a55873cc8b4e7af99
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-18 14:40:26 -0300

    Update

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,19 +5,17 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://localhost:27017/';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
 let db = null;
 
-const connection = () =>
-  db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
-        db = conn.db(DB_NAME);
-        return db;
-      });
+const connection = async () => {
+  const conn = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
+  const db = conn.db(DB_NAME);
+  return db;
+};
 
 module.exports = connection;
