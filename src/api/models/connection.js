c9801897e75b016b202fbd1e6cbe407d168bb1f7
Clênio Galvão <clenio.cgm@gmail.com>
2021-06-20 14:00:40 -0300

    Vira chave

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -5,8 +5,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
 const DB_NAME = 'Cookmaster';
 
 let db = null;
