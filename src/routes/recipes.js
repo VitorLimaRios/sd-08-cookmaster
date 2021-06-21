ad4e7e5cd704777aade85effbe69537b7d098a5a
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-06-20 22:33:37 -0300

    testando auth

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -7,9 +7,7 @@ const app = express();
 
 app.get('/:id', recipes.getRecipeById);
 app.get('/', recipes.getRecipes);
-
-app.use(validateJWT);
-app.post('/', recipes.createRecipe);
+app.post('/', validateJWT, recipes.createRecipe);
 
 module.exports = app;
 
