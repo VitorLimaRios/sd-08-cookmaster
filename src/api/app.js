d7668ad7d5b9b2cd3f68ae7cfc9a083a8c913afa
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-06-21 15:44:45 -0300

    Req 10 done

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { userRouter, loginRouter, recipeRouter } = require('../routes');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use('/login', loginRouter);
 app.use('/recipes', recipeRouter);
 
 app.use(express.static(`${__dirname}/uploads`));
+
+// following line found in the readme
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
