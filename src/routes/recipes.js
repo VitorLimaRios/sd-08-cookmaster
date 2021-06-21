0d8594aa8dedb6c98790bbacde1e6877fa3db002
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-06-20 22:18:09 -0300

    testando 4 e 5

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,8 +5,8 @@ const validateJWT = require('../api/auth/validateJWT');
 
 const app = express();
 
-app.get('/', recipes.getRecipes);
 app.get('/:id', recipes.getRecipeById);
+app.get('/', recipes.getRecipes);
 
 app.use(validateJWT);
 app.post('/', recipes.createRecipe);
