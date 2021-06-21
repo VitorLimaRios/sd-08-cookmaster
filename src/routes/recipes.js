70fb7d08436ca0b0eb2a6bfcfc65195057cfef8e
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-06-20 22:40:38 -0300

    voltando para requisito 3

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,8 +5,8 @@ const validateJWT = require('../api/auth/validateJWT');
 
 const app = express();
 
-app.get('/:id', recipes.getRecipeById);
-app.get('/', recipes.getRecipes);
+// app.get('/', recipes.getRecipes);
+// app.get('/:id', recipes.getRecipeById);
 app.post('/', validateJWT, recipes.createRecipe);
 
 module.exports = app;
