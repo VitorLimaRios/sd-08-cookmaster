0a37e9f1d8837dfa577c8f4daf436995711630d6
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-06-20 22:52:58 -0300

    Revert "testando 4 e 5"
    This reverts commit 0d8594aa8dedb6c98790bbacde1e6877fa3db002.

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -5,8 +5,8 @@ const validateJWT = require('../api/auth/validateJWT');
 
 const app = express();
 
-app.get('/:id', recipes.getRecipeById);
 app.get('/', recipes.getRecipes);
+app.get('/:id', recipes.getRecipeById);
 
 app.use(validateJWT);
 app.post('/', recipes.createRecipe);
