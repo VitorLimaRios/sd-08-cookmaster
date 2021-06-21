03e36573f5ce073dd7494f234dca358988078a20
Bruno Wesley <bruno.wesley77@gmail.com>
2021-06-20 21:35:48 -0300

    req 2 again

--- src/api/app.js
+++ src/api/app.js
@@ -14,6 +14,6 @@ app.get('/', (request, response) => {
 // Não remover esse end-point, ele é necessário para o avaliador
 
 app.use('/users', userController);
-//app.use('/login', loginController);
+app.use('/login', loginController);
 
 module.exports = app;
--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,13 +1,13 @@
 const loginModel = require('../models/loginModel');
 
-const getAll = async (dataUser) => {
+const getAll = async (dataUsers) => {
   const getAll = await loginModel.getAll();
   const emailExist = getAll.some((data) => 
-    data.email === dataUser.email
+    data.email === dataUsers.email
   );
 
   const passwordExist = getAll.some((data) => 
-    data.password === dataUser.password
+    data.password === dataUsers.password
   );
 
   if(!emailExist || !passwordExist) return { message: 'Incorrect username or password'};
