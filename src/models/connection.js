451558689ee2d1117207a21b3e9082210384e0b7
Silvio Dayube <scarige@gmail.com>
2021-06-21 01:32:57 -0300

    fix: Change connection to work on evaluator db

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // usar no db local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // usar no avaliador
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; // usar no db local
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // usar no avaliador
 
 const DB_NAME = 'Cookmaster';
 const OPTIONS = {
