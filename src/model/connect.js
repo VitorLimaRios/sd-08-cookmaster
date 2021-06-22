06fa8e23c88447046abd83582d5a93f1b0452119
Blucilha <blucilha.nascimento@gmail.com>
2021-06-22 00:02:33 -0300

    desafio 5 correçao url

--- src/model/connect.js
+++ src/model/connect.js
@@ -5,12 +5,12 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 // Para teste local
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
 
 // Para subir
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 let db = null;
 
