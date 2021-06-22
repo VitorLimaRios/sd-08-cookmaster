209da7e2fe50fa26087159f4b9e0e0621532d123
Erling <erlingjunior@gmail.com>
2021-06-21 21:35:32 -0300

    mudando para server remoto

--- src/models/connection.js
+++ src/models/connection.js
@@ -7,9 +7,12 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+/* teste local */
+/* const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster'; */
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster'; // teste remoto
+
+/* teste remoto */
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 let db = null;
 
