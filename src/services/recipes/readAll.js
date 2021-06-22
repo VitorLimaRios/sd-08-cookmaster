f93786e2ec87443bb6a00dc29393ce9d870ab948
hugobrr <hugobrr@gmail.com>
2021-06-22 08:22:19 -0300

    refaznedo o package

--- src/services/recipes/readAll.js
+++ src/services/recipes/readAll.js
@@ -1,6 +1,6 @@
 const read = require('../../models/read');
 const HTTP_OK_STATUS = 200;
-const readProductsById = async (_req, res, next) => {
+const readById = async (_req, res, next) => {
   let result = {};
   console.log('leitura');
   result = await read.getAll('recipes');
@@ -8,4 +8,4 @@ const readProductsById = async (_req, res, next) => {
   res.status(HTTP_OK_STATUS).json( result);
   next();
 };
-module.exports = readProductsById;
+module.exports = readById;
