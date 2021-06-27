fcb545a041e26afcd33c64c3494ced666a772158
wagnerberna <cabelo29007@gmail.com>
2021-06-27 07:39:23 -0300

    teste-upload

---
+++ src/api/controllers/userController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const userModel = require('../models/userModel');
+const userService = require('../services/userService');
+const {} = userService;
+
+const STATUS_OK = 200;
+const STATUS_CREATED = 201;
+const ERROR_SERVER = 500;
+const messageErrorServer = {message: 'Sistema Indisponível'};
+
+router.post('/', async(req, res) =>{
+  const {name, email, password} = req.body;
+  try {
+    const result = await userModel.addUser(name, email, password);
+    res.status(STATUS_CREATED).json(result);
+  } catch (error) {
+    console.error(error.message);
+    res.status(ERROR_SERVER).send(messageErrorServer);
+  }
+});
+
+module.exports = router;
