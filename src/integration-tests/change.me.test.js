146f2b0625c6b5c7949ffdd8693bc535644af31f
caiomzrocha <caiomzrocha@gmail.com>
2021-07-01 23:59:18 -0300

    [REQ-11] Adiciona um teste para criar um usuário.

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,37 @@
-describe('', () => {
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const { expect } = chai;
+const server = require('../api/app');
+
+describe('POST /users', () => {
+  describe('Quando não é passado o campo "name" do usuário', () => {
+    let response;
+    before(async () => {
+      response = await chai.request(server)
+        .post('/users')
+        .send({
+          email: 'email@email.com',
+          password: 'password',
+        });
+    });
+
+    // after(() => {});
+
+    it('retorna o código de status "400"', () => {
+      expect(response).to.have.status(400);
+    });
+
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('objeto de respoasta possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it('a propriedade message possui a seguinte frase "Invalid entries. Try again."', () => {
+      expect(response.body.message).to.be.equals('Invalid entries. Try again.');
+    });
+  });
 });
