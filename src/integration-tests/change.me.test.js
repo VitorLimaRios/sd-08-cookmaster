a01492dde25d9385cab03115195451d05c1de200
hugobrr <hugobrr@gmail.com>
2021-07-02 14:59:33 -0300

    Vamos para os 2 testes.

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,16 +1,17 @@
 // https://github.com/tryber/sd-08-cookmaster/blob/e829394cd1264c2b2e1de4680e9b4a2996a4fa7e/src/integration-tests/change.me.test.js
-// inspirado no codigo do caio
+// inspirado no codigo do Caio
 const chai = require('chai');
 const sinon = require('sinon');
 const chaiHttp = require('chai-http');
 const server = require('../api/app');
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const deleteOne = require('../models/delete');
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('POST /users', () => {
+describe.only('POST /users', () => {
   describe('Será validado que o campo "name" é obrigatório', () => {
     let response;
 
@@ -102,62 +103,71 @@ describe('POST /users', () => {
       expect(response.body.message).to.be.equals('Invalid entries. Try again.');
     });
   });
-//   describe('Será validado que é possível cadastrar usuário com sucesso', () => {
-//     let response;
-//     const DBServer = new MongoMemoryServer();
+  describe('Será validado que é possível cadastrar usuário com sucesso', () => {
+    let response;
+    const DBServer = new MongoMemoryServer();
 
-//     before(async () => {
-//       // const URLMock = await DBServer.getUri();
-//       // const OPTIONS = {
-//       //   useNewUrlParser: true,
-//       //   useUnifiedTopology: true,
-//       // };
+    before(async () => {
+      // const URLMock = await DBServer.getUri();
+      // const OPTIONS = {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // };
 
-//       // const connectionMock = await MongoClient.connect(URLMock, OPTIONS);
+      // const connectionMock = await MongoClient.connect(URLMock, OPTIONS);
+     console.log('entrei no before');
+      const connectionMock = await DBServer.getUri()
+      .then(URLMock => MongoClient.connect(
+          URLMock,
+          { useNewUrlParser: true, useUnifiedTopology: true }
+      ))
+      .catch(err => {
+          console.log(err)
+          done(err)
+      });
 
-//       const connectionMock = await DBServer.getUri()
-//       .then(URLMock => MongoClient.connect(
-//           URLMock,
-//           { useNewUrlParser: true, useUnifiedTopology: true }
-//       ));
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
 
-//       sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+      connectionMock.db('Cookmaster')
+        .collection('users')
+        .insertOne({
+            name: 'string',
+            email: 'email@email.com',
+            password: 'string',
+          });
 
-//       connectionMock.db('Cookmaster')
-//         .collection('users')
-//         .insertOne({
-//             name: 'string',
-//             email: 'email@email.com',
-//             password: 'string',
-//           });
+      response = await chai.request(server)
+        .post('/users')
+        .send({
+          name: 'string',
+          email: 'email@email.com',
+          password: 'string',
+        });
+        console.log(response);
+    });
 
-//       response = await chai.request(server)
-//         .post('/users')
-//         .send({
-//           name: 'string',
-//           email: 'email@email.com',
-//           password: 'string',
-//         });
-//         console.log(response);
-//     });
-
-//     // after(() => {});
-//     it('existe uma resposta', () => {
-//         console.log(response,'teste1');
+    after(async() => {
+        console.log('entrei no after');
+        console.log('true ', DBServer.getInstanceInfo());
+        await DBServer.stop();
+        console.log('False ', DBServer.getInstanceInfo());
+    });
+    it('existe uma resposta', () => {
+        console.log(response,'teste1');
     
-//           expect(response).to.exist();
-//         console.log(response,'teste2');
+          expect(response).to.exist();
+        console.log(response,'teste2');
 
-//         });
+        });
     
-//     console.log(response,'teste3');
-//     it('retorna o código de status "201"', () => {
-//     console.log(response,'teste4');
+    console.log(response,'teste3');
+    it('retorna o código de status "201"', () => {
+    console.log(response,'teste4');
 
-//       expect(response).to.have.status(201);
-//     });
+      expect(response).to.have.status(201);
+    });
 
-//   });
+  });
 //   Será validado que é possível ao cadastrar usuário, o valor do campo "role" tenha o valor "user"
     // describe('Será validado que é possível ao cadastrar usuário, o valor do campo "role" tenha o valor "user"', () => {
     // let response;
