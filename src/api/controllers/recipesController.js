adf80fbe71f08e379d2e97dcca81cbfed4ddf917
Victor Breno <victor.brns2@gmail.com>
2021-07-08 17:30:49 -0300

    requisito 5

--- src/api/controllers/recipesController.js
+++ src/api/controllers/recipesController.js
@@ -6,8 +6,30 @@ const recipesModel = require('../models/recipeModel');
 
 const invalidEntriesStatus = 400;
 const invalidTokenStatus = 401;
+const failRecipeStatus = 404;
 const sucessStatus = 201;
 const sucessAllRecipesStatus = 200;
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const token = req.headers.authorization;
+  const recipeById = await recipesModel.getById(id);
+
+  if (token) {
+    const authorizatedToken = await recipesService.authToken(token);
+    if (!token || authorizatedToken === 'err') {
+      return res.status(invalidTokenStatus).send({ message: 'jwt malformed' });
+    }
+  }
+
+  if (recipeById) {
+    return res.status(sucessAllRecipesStatus).json(recipeById);
+  };
+
+  return res.status(failRecipeStatus).json({ 
+    message: 'recipe not found'
+  });
+});
 
 router.get('/', async (req, res) => {
   const token = req.headers.authorization;
