523d0255f46e13d1ac4d044b71d1ff97de823261
Victor Breno <victor.brns2@gmail.com>
2021-07-09 09:47:14 -0300

    corrige connection

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -6,21 +6,23 @@ const OPTIONS = {
 };
 
 // LOCAL
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
-
-// AVALIADOR
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 // const DB_NAME = 'Cookmaster';
 
-let db = null;
+// AVALIADOR
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+// let db = null;
 
 const connection = () => {
-  return db ? Promise.resolve(db) : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  return MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => {
       db = conn.db(DB_NAME);
       return db;
     });
 };
+
+// db ? Promise.resolve(db) : 
 
 module.exports = connection;
